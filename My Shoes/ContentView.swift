1cce32b8e769bf17027d70dc54a5a7b4a18d951a
Caio Mori <caiomori22@icloud.com>
2023-06-22 17:52:26 -0300

    Initial Commit

---
+++ My Shoes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  My Shoes
+//
+//  Created by Caio Hideki Martins Alves Mori on 22/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ My Shoes/My_ShoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_ShoesApp.swift
+//  My Shoes
+//
+//  Created by Caio Hideki Martins Alves Mori on 22/06/23.
+//
+
+import SwiftUI
+
+@main
+struct My_ShoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
