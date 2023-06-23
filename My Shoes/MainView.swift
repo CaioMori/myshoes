3d02f22d17f26345d94aa8a6e046116eed957c57
Caio Mori <caiomori22@icloud.com>
2023-06-23 00:19:13 -0300

    feat(main): add header and footer

--- My Shoes/MainView.swift
+++ My Shoes/MainView.swift
@@ -9,13 +9,85 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Olá Mundo!")
+        VStack(spacing: 0) {
+            HeaderView()
+            FooterView()
+            // Restante do conteúdo da tela
+        }
+    }
+}
+
+struct HeaderView: View {
+    var body: some View {
+        HStack {
+            // Menu hamburguer na esquerda
+            Image(systemName: "line.horizontal.3")
+                .font(.title)
+                .foregroundColor(.white)
+            
+            Spacer()
+            
+            Text("My Shoes")
+                .font(Font.custom("Montserrat", size: 24))
+                .bold()
+                .foregroundColor(.white)
+            
+            Spacer()
+            
+            Circle()
+                .frame(width: 32, height: 32)
+                .foregroundColor(.gray)
         }
         .padding()
+        .background(Color(red: 1, green: 0.65, blue: 0.15))
+    }
+}
+
+struct FooterView: View {
+    var body: some View {
+        VStack{
+            
+            HStack{
+                Text("Contato:")
+                Spacer()
+                Text("(xx) xxxxxx-xxxx")
+            }
+            
+            HStack{
+                Text("Atendimento 24h:")
+                Spacer()
+                Text("(xx) xxxxxx-xxxx")
+            }
+            
+            HStack{
+                Text("Atendimento via e-mail:")
+                Spacer()
+                Text("contato@myshoes.com")
+                    .accentColor(.black)
+            }
+            
+            HStack{
+                VStack{
+                    Text("Parceiros")
+                        .font(.body)
+                        .fontWeight(.bold)
+                    HStack{
+                        Image("compost_FILL0_wght400_GRAD0_opsz48 2")
+                            .frame(width: 30, height: 29)
+                        Image("lightbulb_circle_FILL0_wght400_GRAD0_opsz48 2")
+                            .frame(width: 30, height: 29)
+                    }
+                }
+                
+                Spacer()
+                    .padding()
+
+            }
+            
+        }
+        .font(.callout)
+        .padding()
+        .background(Color(red: 1, green: 0.65, blue: 0.15))
     }
 }
 
